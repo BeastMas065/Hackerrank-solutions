3ddcd08f260d01d77aa9c194e94ff3ed96836d1b
Aqheel <aqheelsharief@gmail.com>
2026-05-20 16:34:24 +0530

    update

---
+++ python fundamentals/email.py
@@ -0,0 +1,16 @@
+import re
+def fun(s):
+    return(bool(re.fullmatch(r'[A-Za-z0-9_-]+@[A-Za-z0-9]+\.[A-Za-z]{1,3}',s)))
+
+def filter_mail(emails):
+    return list(filter(fun, emails))
+
+if __name__ == '__main__':
+    n = int(input())
+    emails = []
+    for _ in range(n):
+        emails.append(input())
+
+filtered_emails = filter_mail(emails)
+filtered_emails.sort()
+print(filtered_emails)

288e407102d5cdaf2f98a16ea80030d8bb02eaea
Aqheel <aqheelsharief@gmail.com>
2026-05-22 10:26:12 +0530

    update

---
+++ regex and parsing/matrix_script.py
@@ -0,0 +1,15 @@
+import math
+import re
+first_multiple_input = input().rstrip().split()
+n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+matrix = list(zip(*matrix))
+h = ''
+for i in matrix:
+    h += ''.join(i)
+print(re.sub(r"(?<=[a-zA-Z0-9])[ #$%!@&]+(?=[a-zA-Z0-9])"," ",h))

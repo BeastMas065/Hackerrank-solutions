17a0395a2cd586d6a3351fe18e02c7f6aa77e9f2
Aqheel <aqheelsharief@gmail.com>
2026-05-27 13:44:12 +0530

    update

--- Regex/applications/alien_username.py
+++ Regex/applications/alien_username.py
@@ -1,3 +1,3 @@
 import re
 for _ in range(int(input())):
-    print("VALID" if bool(re.fullmatch(r'[_.][0-9]+[a-zA-Z]*_?',input())) else "INVALID")
+    print("VALID" if (re.fullmatch(r'[_.][0-9]+[a-zA-Z]*_?',input())) else "INVALID")

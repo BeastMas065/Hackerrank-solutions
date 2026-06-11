4ce448e778ad204bb96181a5d35f312f1d9dfd83
Aqheel <aqheelsharief@gmail.com>
2026-06-11 23:25:18 +0530

    update

---
+++ Algorithms/implementation/migotory_birds.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    ans = min(arr)
+    c = 0
+
+    for i in set(arr):
+        count = arr.count(i)
+
+        if count > c:
+            c = count
+            ans = i
+        elif count == c and i < ans:
+            ans = i
+
+    return ans
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

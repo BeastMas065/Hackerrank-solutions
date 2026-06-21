3d629a8d69959825f9059c4462121262a1bfb3c9
Aqheel <aqheelsharief@gmail.com>
2026-06-21 18:10:36 +0530

    update

---
+++ Algorithms/implementation/Picking_numbers.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+from collections import Counter
+
+def pickingNumbers(a):
+    freq = Counter(a)
+    ans = 0
+
+    for x in freq:
+        ans = max(ans, freq[x] + freq.get(x + 1, 0))
+
+    return ans
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
